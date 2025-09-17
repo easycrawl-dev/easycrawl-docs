172d1f605ea16283349992421eb91df8207f0173
Rabbir 兔子 <m18018230731@163.com>
2025-09-17 01:58:21 +0000

    upgrade: 升级到 Nextra 4.0

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
-const withNextra = require('nextra')({
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.tsx',
+import nextra from 'nextra'
+ 
+// Set up Nextra with its configuration
+const withNextra = nextra({
+  // ... Add Nextra-specific options here
+  contentDirBasePath: '/content',
+  search: true
 })
-
-module.exports = withNextra()
+ 
+// Export the final Next.js config with Nextra included
+export default withNextra({
+  // ... Add regular Next.js options here
+})
